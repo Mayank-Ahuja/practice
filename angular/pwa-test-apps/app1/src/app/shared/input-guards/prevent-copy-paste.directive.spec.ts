eae6895cbea25beb40f63da1ae9967ca27e13cc1
Mayank Ahuja <mayankahuja8894@gmail.com>
2022-08-29 20:46:25 +0530

    removed unwanted apps

--- angular/pwa-test-apps/app1/src/app/shared/input-guards/prevent-copy-paste.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PreventCopyPasteDirective } from './prevent-copy-paste.directive';
-
-describe('PreventCopyPasteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PreventCopyPasteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- angular/pwa-test-apps/app1/src/app/shared/pipes/by-prop.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ByPropPipe } from './by-prop.pipe';
-
-describe('ByPropPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ByPropPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
